5216af05522c3e32adf6f00d8678cf7b7e2b1eff
Karen Asipuela Paredes <karen.asipuela@student.vinci.be>
2024-09-17 16:19:06 +0200

    new:2.1-stored

--- exercises/2.1/src/components/ClickCounter.jsx
+++ exercises/2.1/src/components/ClickCounter.jsx
@@ -1,16 +1,17 @@
 import { useState } from 'react';
 
 const ClickCounter = ({titre, message, messageOver}) => {
-    const [count, setCount] = useState(0);
+    const [count, setCount] = useState(JSON.parse(localStorage.getItem('count')) || 0);
     const [messageOverBool, setMessageOver] = useState(false);
     
     return (
         <div>
         <h2>{titre}</h2>
         {messageOverBool ? <div>{messageOver}</div> : null}
-        <button onClick={() => setCount(count + 1)}
+        <button onClick={() => setCount(count + 1) & localStorage.setItem('count', JSON.stringify(count+1))}
            onMouseOver={() => setMessageOver(!messageOverBool)}
             onMouseOut={() => setMessageOver(!messageOverBool)}
+            // onMouseEnter et onMouseLeave sont aussi des options
            >{count}</button>
         {count > 10 ? <div>{message}</div> : null}
         </div>
