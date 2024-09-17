eb91860ed35aae1797f6887c848db24d6c531a10
Karen Asipuela Paredes <karen.asipuela@student.vinci.be>
2024-09-17 14:48:20 +0200

    1.1-1.2 : refactor - modules

--- exercises/1-1/src/App.jsx
+++ exercises/1-1/src/App.jsx
@@ -1,11 +1,11 @@
-import Header from "./Header"
-import Content from "./Content"
-import Total from "./Total"
+import Header from "./components/Header/Header"
+import Content from "./components/Content/Content"
+import Total from "./components/Total/Total"
 
 const App = () => {
   const course = 'Half Stack application development'
   const part1 = 'Fundamentals of React'
-  const exercises1 = 10
+  const exercises1 = 15
   const part2 = 'Using props to pass data'
   const exercises2 = 7
   const part3 = 'State of a component'
